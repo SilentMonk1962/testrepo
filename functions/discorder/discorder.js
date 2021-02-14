4a9076362712e96e3e96fa731592060b57d7b594
Abhishek Singh Bhilware <abhisheksinghbhilware@Abhisheks-MacBook-Air.local>
2021-02-14 15:28:46 +0530

    5th

--- functions/discorder/discorder.js
+++ functions/discorder/discorder.js
@@ -6,11 +6,15 @@ exports.handler = async (event, context) => {
     const body = JSON.parse(event.body);
     console.log("Passing the JSON object parsed body.");
     console.log(body);
-    const surveyID=body.survey_id;
-    const startTime=body.submission_startTime;
+    const surveyID = body.survey_id;
+    const submissionID = body.submission_id;
+    const C1 = body.C1 || "NA_c1";
+    const C2 = body.C2 || "NA_c2";
+    const C3 = body.C3 || "NA_c3";
+    const Q2 = body.Q2;
     const endTime=body.submission_completedTime;
     const res = await axios.post(process.env.DISCORD_WEBHOOK_URL, {
-      content: `Survey number ${surveyID} started at ${startTime} and ended at ${endTime}.`
+      content: `There is a new submission with ID: ${submissionID} under survey number: ${surveyID}. It was completed on ${endTime}. The customer's reason for not buying is ${Q2}. Additional details of the user are ${C1} ${C2} ${C3}.`
     });
     console.log("Submitted!");
     return {
