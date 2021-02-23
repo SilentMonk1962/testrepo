c802f2eb1a778222364fc8d3a9f07643f531626d
Abhishek Singh Bhilware <abhisheksinghbhilware@Abhisheks-MacBook-Air.local>
2021-02-23 17:56:34 +0530

    fixed params names

--- functions/stopsipdiscorder/stopsipdiscorder.js
+++ functions/stopsipdiscorder/stopsipdiscorder.js
@@ -3,24 +3,16 @@ exports.handler = async (event, context) => {
   try {
     const body = JSON.parse(event.body);
     console.log(body);
-    const userEmail=body.C3;
-    const userName=body.C4;
-    const amountSIP=body.C2;
+    const userEmail = body.C3 || "Email not provided";
+    const userName = body.C4 || "UserName not provided";
+    const amountSIP = body.C2 || "SIP amount not provided";
     const surveyID = body.survey_id;
     const submissionID = body.submission_id;
-    const endTime=body.submission_completedTime;
+    const endTime= body.submission_completedTime;
     const Q1 = body.Q1;
     const Q2 = body.Q2;
         const res = await axios.post(process.env.SS_stop_SIP_DISCORD_WEBHOOK_URL, {
-            content: `
-            New Submission: ${submissionID} from survey: ${surveyID}. 
-            CompletedOn: ${endTime}. 
-            UserName:${userName}.
-            UserEmail: ${userEmail}.
-            Stopped SIP amount: ${C4}.
-            Reason for stopping SIP: ${Q1}.
-            Switching to: ${Q2}.
-            `
+            content: `New Submission: ${submissionID} from survey: ${surveyID}. CompletedOn: ${endTime}. UserName:${userName}. UserEmail: ${userEmail}. Stopped SIP amount: ${amountSIP}. Reason for stopping SIP: ${Q1}. Switching to (if shared): ${Q2}.`
           });
           console.log("Submitted!");
     return {
