12e25a0d7fee485f84eaa776a5bafde489e25e53
silentmonk1962 <abhishek.singh@iiml.org>
2021-02-26 17:04:20 +0530

    19

--- functions/discorder/discorder.js
+++ functions/discorder/discorder.js
@@ -1,23 +1,20 @@
 const axios = require("axios").default;
 const sgMail = require('@sendgrid/mail')
-console.log(process.env.SENDGRID_API_KEY);
 sgMail.setApiKey(process.env.SENDGRID_API_KEY)
-
-// Docs on event and context https://www.netlify.com/docs/functions/#the-handler-method
 exports.handler = async (event, context) => {
   try {
-    const body = JSON.parse(event.body);
+    const body = JSON.parse(event.body) || {};
     //console.log("Passing the JSON object parsed body.");
     //console.log(body);
-    const surveyID = body.survey_id;
-    const submissionID = body.submission_id;
-    const C1 = body.C1 || "NA_c1";
-    const C2 = body.C2 || "NA_c2";
-    const C3 = body.C3 || "NA_c3";
+    const surveyID = body.survey_id || "NA";
+    const submissionID = body.submission_id || "NA";
+    const C1 = body.C1 || "NA";
+    const C2 = body.C2 || "NA";
+    const C3 = body.C3 || "NA";
     const Q2 = body.Q2;
     const endTime=body.submission_completedTime;
     const msg = {
-      to: 'abhishek.singh@iiml.org', // Change to your recipient
+      to: 'abhishek.singh@kuvera.in', // Change to your recipient
       from: 'abhishek.singh@kuvera.in', // Change to your verified sender
       subject: `Group Health CallBackRequest | SubmissionID ${submissionID}`,
       text: `
@@ -48,8 +45,7 @@ exports.handler = async (event, context) => {
             Additional details of the user are ${C1} ${C2} ${C3}.`
           });
           console.log("Submitted!");
-          console.log(msg);
-          sgMail.send(msg).then(() => {console.log('Also sent a mail to support.')}).catch((error) => {console.error(error)});
+          sgMail.send(msg).then(() => {console.log(`Email sent to ${msg.to}.`)}).catch((error) => {console.error(error)});
     }
     else {
         console.log("User did not like the offering.")
